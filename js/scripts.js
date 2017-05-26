55f69cf4189f88154f0fbaf397f77d0605b6f0d1
VEDANT KASHYAP <smartvedi@gmail.com>
2017-05-26 13:55:50 +0530

    Developed the basic front-end of the WebApp and wrote some super basic javascript onclick scripts for buttons and compiled a lost of common usernames at TwitchTV.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 /**
  * Created by VEDANT KASHYAP on 5/26/2017.
  */
+
+var channels=["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+function showAll() {
+    console.log("showing all");
+}
+
+function showOnline() {
+    console.log("showing online");
+}
+
+function showOffline() {
+    console.log("showing offline");
+}
